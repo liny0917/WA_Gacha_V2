9a64b7b4e7a4d18dc4e7fe82c8b2e7c321df8b64
Liny <d.moonlin@gmail.com>
2023-03-31 19:49:20 +0800

    加入專案檔案。

---
+++ WA_Gacha_V2/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace WA_Gacha_V2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonInput = new System.Windows.Forms.Button();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.textBoxState = new System.Windows.Forms.TextBox();
+            this.buttonRefund = new System.Windows.Forms.Button();
+            this.buttonShipped = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonInput
+            // 
+            this.buttonInput.Font = new System.Drawing.Font("新細明體", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.buttonInput.Location = new System.Drawing.Point(770, 68);
+            this.buttonInput.Name = "buttonInput";
+            this.buttonInput.Size = new System.Drawing.Size(100, 39);
+            this.buttonInput.TabIndex = 0;
+            this.buttonInput.Text = "投幣";
+            this.buttonInput.UseVisualStyleBackColor = true;
+            this.buttonInput.Click += new System.EventHandler(this.buttonInput_Click);
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(83, 68);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(648, 29);
+            this.textBoxInput.TabIndex = 1;
+            this.textBoxInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBoxState
+            // 
+            this.textBoxState.Location = new System.Drawing.Point(79, 140);
+            this.textBoxState.Multiline = true;
+            this.textBoxState.Name = "textBoxState";
+            this.textBoxState.Size = new System.Drawing.Size(652, 189);
+            this.textBoxState.TabIndex = 2;
+            // 
+            // buttonRefund
+            // 
+            this.buttonRefund.Font = new System.Drawing.Font("新細明體", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.buttonRefund.Location = new System.Drawing.Point(511, 360);
+            this.buttonRefund.Name = "buttonRefund";
+            this.buttonRefund.Size = new System.Drawing.Size(100, 39);
+            this.buttonRefund.TabIndex = 3;
+            this.buttonRefund.Text = "退幣";
+            this.buttonRefund.UseVisualStyleBackColor = true;
+            this.buttonRefund.Click += new System.EventHandler(this.buttonRefund_Click);
+            // 
+            // buttonShipped
+            // 
+            this.buttonShipped.Font = new System.Drawing.Font("新細明體", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.buttonShipped.Location = new System.Drawing.Point(631, 360);
+            this.buttonShipped.Name = "buttonShipped";
+            this.buttonShipped.Size = new System.Drawing.Size(100, 39);
+            this.buttonShipped.TabIndex = 4;
+            this.buttonShipped.Text = "出貨";
+            this.buttonShipped.UseVisualStyleBackColor = true;
+            this.buttonShipped.Click += new System.EventHandler(this.buttonShipped_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(909, 439);
+            this.Controls.Add(this.buttonShipped);
+            this.Controls.Add(this.buttonRefund);
+            this.Controls.Add(this.textBoxState);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.buttonInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonInput;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.TextBox textBoxState;
+        private System.Windows.Forms.Button buttonRefund;
+        private System.Windows.Forms.Button buttonShipped;
+    }
+}
+
---
+++ WA_Gacha_V2/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace WA_Gacha_V2
+{
+    public partial class Form1 : Form
+    {
+        public const int Price = 50;//商品售價
+        public const string Pattern = @"^[0-9]*$";
+        private int _total = 0;
+        private int _input;
+        FormViewModel _model;
+        public Form1()
+        {
+            //_total = total;
+            InitializeComponent();
+            DisableButton();
+        }
+
+        private int Input
+        {
+            get
+            {
+                Int32.TryParse(textBoxInput.Text, out _input);
+                if (Regex.IsMatch(_input.ToString(), Pattern))
+                {
+                    return _input;
+                }
+                else
+                {
+                    return 0;
+                }
+                //   if (isInput)
+       
+                //return 0;
+            }
+            set { textBoxInput.Text = value.ToString(); }
+        }
+
+        //public object input { get; private set; }
+
+        private void buttonInput_Click(object sender, EventArgs e)
+        {
+         //   FormViewModel model;//=new FormViewModel();
+            try
+            {
+                _model = new FormViewModel(this.Input);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+            _total += _model.Money;
+            GenerateStatus();
+            //return model;
+        }
+
+        private void GenerateStatus()
+        {
+            string message;
+            DisableButton();
+            if (_total > 0)
+            {
+                if (_total < Price)
+                {
+                    buttonRefund.Enabled = true;
+                    message = _model.State;
+                }
+                else
+                {
+                    EnabledButton();
+                    message = _model.State;
+                }
+            }
+            else
+            {
+                message = _model.State;
+            }
+            SetMessage(message);
+        }
+
+        private void SetMessage(string message)
+        {
+            textBoxState.Text = message;
+        }
+
+        private void EnabledButton()
+        {
+            buttonRefund.Enabled = true;
+            buttonShipped.Enabled = true;
+        }
+
+        private void DisableButton()
+        {
+            buttonRefund.Enabled = false;
+            buttonShipped.Enabled = false;
+        }
+
+        private void buttonRefund_Click(object sender, EventArgs e)
+        {
+          //  FormViewModel model;
+            textBoxInput.Text = "";
+            _total = 0;
+            GenerateReturnStatus();
+        }
+
+        private void GenerateReturnStatus()
+        {
+           // FormViewModel model = null;
+            MessageBox.Show(@"已完成退幣作業");
+            DisableButton();
+            string message = _model.State;
+            SetMessage(message);
+        }
+
+        private void buttonShipped_Click(object sender, EventArgs e)
+        {
+            _total -= Price;
+            GenerateStatus();
+        }
+    }
+
+    public class FormViewModel
+    {
+        //private string state;
+        //private int money;
+
+        public string State { get; set; }
+        public int Money { get; set; }
+
+        public FormViewModel(int money)
+        {
+            if (money <= 0)
+            {
+                var text = string.Format(@"State:StandbyState 機器沒在工作,等待下一個客人操作\r\nMoney:{0}", money);
+                State =text.Replace("\\r\\n",Environment.NewLine);
+                //State = string.Format(@"State:StandbyState 機器沒在工作,等待下一個客人操作"+Environment.NewLine+@"Money:{0}", money);
+                //State = string.Format(@"State:StandbyState 機器沒在工作,等待下一個客人操作</br>Money:{0}", money).Replace("\\r\\n",Environment.NewLine);
+                Money = money;
+               // throw new ArgumentOutOfRangeException(money.ToString());
+            }
+            if (money < Form1.Price)
+            {
+                State = string.Format(@"State:InsufficientAmountState 有投幣,但金額還不足夠\r\nMoney:{0}", money);
+                //State = state;
+                Money = money;
+            }
+            else
+            {
+                State = string.Format(@"State:ReadyToShip 投入足夠金額,等待客人指令就出貨\r\nMoney:{0}", money);
+                //State = state;
+                Money = money;
+            }
+        }
+
+        //public decimal Price { get; private set; }
+
+        //public decimal Price
+        //{
+        //    get { return Price; }
+        //}
+    }
+}
